0e59a55533e2140a38a2893368aa2ae9360e5e6c
mdahamshi <mmd.0@hotmail.com>
2017-05-24 15:02:02 +0300

    some security fixes..

--- pushBullet.sh
+++ pushBullet.sh
@@ -5,5 +5,5 @@ if [ -z "$2" ]
 then
 	b=$1
 fi
-curl -u """o.ejxWal6kxkNJ2IjueAIbHYbEcusdkYZP"":" -d type="note" -d body="$b" -d title="$t" -d email="mmd.robot@gmail.com" 'https://api.pushbullet.com/v2/pushes' 
+curl -u """$(cat /data/aes/key)"":" -d type="note" -d body="$b" -d title="$t" -d email="mmd.robot@gmail.com" 'https://api.pushbullet.com/v2/pushes' 
 exit 0
