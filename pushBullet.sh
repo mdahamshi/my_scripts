c5e1cd6d6f4081e526261d85d5e7dbd6b390d5bd
mdahamshi <mmd.0@hotmail.com>
2017-07-03 16:50:02 +0300

    added ant camera script

--- pushBullet.sh
+++ pushBullet.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-t=$1
-b=$2
+t="$1"
+b="$2"
 if [ -z "$2" ]
 then
-	b=$1
+	b="$1"
 fi
-curl -u """$(cat /data/aes/key)"":" -d type="note" -d body="$b" -d title="$t" -d email="mmd.robot@gmail.com" 'https://api.pushbullet.com/v2/pushes' 
+curl -u """$(cat /data/aes/key)"":" -d type="note"  -d title="$t" -d email="mmd.robot@gmail.com" 'https://api.pushbullet.com/v2/pushes' 
 exit 0
